6b034ae49c88151aa630227de45c28310ff809d1
emirhan.karagozoglu <emirhan.karagozoglu@accenture.com>
2020-07-19 17:46:28 +0300

    Controlled a servo motor

---
+++ servo/servo_control_with_ui.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 50)
+pwm.start(0)
+GPIO.setwarnings(False)
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        scale = Scale(frame, from_=0, to=180, orient=HORIZONTAL, command=self.update)
+        scale.grid(row=0)
+    
+    def update(self, angle):
+        duty = float(angle) / 10.0 + 2.5
+        pwm.ChangeDutyCycle(duty)
+        
+root = Tk()
+root.wm_title('Servo Control')
+app = App(root)
+root.geometry("200x50+0+0")
+root.mainloop()
