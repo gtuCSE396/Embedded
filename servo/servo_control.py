34669013822abd09eae2727f9fed8653bfebeee1
karginbilgehan <abilgehankargin@gmail.com>
2020-07-30 20:05:15 +0300

    Add new motion according to given angle

--- servo/servo_control.py
+++ servo/servo_control.py
@@ -1,32 +1,29 @@
 import RPi.GPIO as GPIO
 import time
-
-servoPIN = 18
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
-p.start(0) # Initialization
+motor1 = GPIO.PWM(18,50)
+motor2 = GPIO.PWM(12,50)
 
-try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(15)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(25)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(40)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(25)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(15)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(0)
-    time.sleep(0.5)
+def duty(angle):
+    return(float(angle) / 18.0 + 2)
+
+motor1.start(duty(0))
+motor2.start(duty(0))
+
+motor1.ChangeDutyCycle(duty(0))
+motor2.ChangeDutyCycle(duty(0))
+for i in range(0,20):
+    #print ("angle:",i)
+    motor1.ChangeDutyCycle(duty(0))
+    motor2.ChangeDutyCycle(duty(0))
+    time.sleep(0.2)
+    motor1.ChangeDutyCycle(duty(50))
+    motor2.ChangeDutyCycle(duty(50))
+    time.sleep(0.2)
     
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+motor1.stop()
+motor2.stop()
+GPIO.cleanup()
